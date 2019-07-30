73e14876d45b053e96590e5b2c81899936a25bdd
Richard Lee <richlee81192@gmail.com>
2019-07-30 18:16:23 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -6,8 +6,11 @@ end
 def monopoly_with_second_tier
 	monopoly = 
 	{ :railroads => 
-	    {:pieces => 4, 
-	     :name => } 
+	    {
+	      :pieces => 4, 
+	      :name => {}
+	      
+	    } 
 	  
 	}
 
